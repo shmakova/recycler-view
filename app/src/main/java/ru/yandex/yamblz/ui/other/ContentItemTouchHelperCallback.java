70a72adfac996532fa18bda9150aa37269b64e27
shmakova <shmakova-nastya@yandex.ru>
2016-07-26 17:42:50 +0300

    red background on swipe

--- app/src/main/java/ru/yandex/yamblz/ui/other/ContentItemTouchHelperCallback.java
+++ app/src/main/java/ru/yandex/yamblz/ui/other/ContentItemTouchHelperCallback.java
@@ -1,7 +1,11 @@
 package ru.yandex.yamblz.ui.other;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 import ru.yandex.yamblz.ui.adapters.ItemTouchHelperAdapter;
 
@@ -12,16 +16,19 @@ import ru.yandex.yamblz.ui.adapters.ItemTouchHelperAdapter;
 
 public class ContentItemTouchHelperCallback extends ItemTouchHelper.Callback {
     private final ItemTouchHelperAdapter adapter;
+    private Paint redPaint;
 
     public ContentItemTouchHelperCallback(ItemTouchHelperAdapter adapter) {
         this.adapter = adapter;
+        redPaint = new Paint();
+        redPaint.setColor(Color.RED);
     }
     @Override
     public int getMovementFlags(RecyclerView recyclerView,
                                 RecyclerView.ViewHolder viewHolder) {
         int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN |
                 ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
-        int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+        int swipeFlags = ItemTouchHelper.END;
         return makeMovementFlags(dragFlags, swipeFlags);
     }
 
@@ -47,5 +54,22 @@ public class ContentItemTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public boolean isItemViewSwipeEnabled() {
         return true;
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            View itemView = viewHolder.itemView;
+            float width = (float) itemView.getWidth();
+            float alpha = Math.abs(dX) / width;
+            itemView.setTranslationX(dX);
+            redPaint.setAlpha((int) (alpha * 255));
+            c.drawRect((float) itemView.getLeft(), (float) itemView.getTop(), dX,
+                        (float) itemView.getBottom(), redPaint);
+        } else {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY,
+                    actionState, isCurrentlyActive);
+        }
     }
 }
