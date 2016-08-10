360f70c3256ea13735386a631c8011c109d5ea2e
shmakova <shmakova-nastya@yandex.ru>
2016-08-10 03:14:53 +0300

    removed getDecorated

--- app/src/main/java/ru/yandex/yamblz/ui/other/ContentItemDecoration.java
+++ app/src/main/java/ru/yandex/yamblz/ui/other/ContentItemDecoration.java
@@ -14,7 +14,6 @@ public class ContentItemDecoration extends RecyclerView.ItemDecoration {
     private static final int OFFSET = 16;
     private Paint paintBorder;
 
-
     public ContentItemDecoration() {
         paintBorder = new Paint(Paint.ANTI_ALIAS_FLAG);
         paintBorder.setColor(Color.DKGRAY);
@@ -25,19 +24,18 @@ public class ContentItemDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
         super.onDrawOver(c, parent, state);
-        final RecyclerView.LayoutManager layoutManager = parent.getLayoutManager();
+        final int childCount = parent.getChildCount();
 
-        for (int i = 0; i < parent.getChildCount(); i++) {
+        for (int i = 0; i < childCount; i++) {
             final View child = parent.getChildAt(i);
             int position = parent.getChildAdapterPosition(child);
 
             if (position % 2 == 0) {
-                c.drawRect(
-                        layoutManager.getDecoratedLeft(child) + OFFSET / 2,
-                        layoutManager.getDecoratedTop(child) + OFFSET / 2,
-                        layoutManager.getDecoratedRight(child) - OFFSET / 2,
-                        layoutManager.getDecoratedBottom(child) - OFFSET / 2,
-                        paintBorder);
+                final int left = child.getLeft() + OFFSET / 2;
+                final int top = child.getTop() + OFFSET / 2;
+                final int right = child.getRight() - OFFSET / 2;
+                final int bottom = child.getBottom() - OFFSET / 2;
+                c.drawRect(left, top, right, bottom, paintBorder);
             }
         }
     }
